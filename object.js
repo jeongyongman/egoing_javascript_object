02a35bc0b2eac5ab38503d6e17c52d58b6118043
jeongyongman <i1683u@gmail.com>
2021-10-18 08:00:01 +0900

    001_object

---
+++ object.js
@@ -0,0 +1,13 @@
+let memberArray = ['egoing', 'graphittie','leezhce'];
+console.log("memberArray[2]",memberArray[2]);
+
+let memberObject = {
+    manager: 'egoing',
+    developer: 'graphittie',
+    designer: 'leezhce'
+}
+memberObject.designer = 'leezche';
+console.log("memberobject.designer",memberObject.designer);
+console.log("memberObject['designer']",memberObject['designer']);
+delete memberObject.manager;
+console.log('after delete memberObject.manager',memberObject.manager);
