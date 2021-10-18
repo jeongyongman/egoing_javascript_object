bbc9aa94bd865c18a5b7cd93f6bcfe4b92760a9c
jeongyongman <i1683u@gmail.com>
2021-10-18 14:16:38 +0900

    prototype end

--- prototype.js
+++ prototype.js
@@ -3,16 +3,13 @@ function Person(name,first,second,third){
     this.first= first;
     this.second= second;
     this.third= third;
-    // sum:function(f,s){
-    //     return f+s;
-    // }
-    this.sum= function(){
-        return this.first+this.second+this.third;
-    }
+}
+Person.prototype.sum = function(){
+    return 'prototype : '+(this.first+this.second+this.third);
 }
 let kim = new Person('kim', 10, 20, 30);
 kim.sum = function(){
-    return 'modified :'+(this.first+this+this.second);
+    return 'this : '+(this.first+this.second+this.third);
 }
 let lee = new Person('lee', 10, 10, 10);
 console.log("kim.sum()",kim.sum());
