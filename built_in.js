e77f6aa85cd75f0994cf225173e92a7bf64d9c02
jeongyongman <i1683u@gmail.com>
2021-10-18 09:03:57 +0900

    MyMath

--- built_in.js
+++ built_in.js
@@ -1,3 +1,26 @@
 console.log("Math.PI",Math.PI);
-console.log("Math.random()",Math.random());
+console.log("Math.random()",Math.random()); // method
 console.log("Math.floor(3.9)",Math.floor(3.9));
+
+let MyMath = {
+    PI: Math.PI,
+    random: function(){
+        return Math.random();
+    },
+    floor: function(val){
+        return Math.floor(val);
+    }
+}
+console.log("MyMath.PI",MyMath.PI);
+console.log("MyMath.random()",MyMath.random());
+console.log("MyMath.floor(3.9)",MyMath.floor(3.9));
+
+let MyMath_PI = MyMath.PI;
+function MyMath_random(){
+    return MyMath.random();
+}
+function MyMath_floor(val){
+    return MyMath.floor(val);
+}
+console.log(MyMath.random());
+console.log(MyMath.floor(20.1));
