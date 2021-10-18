f8902dd23fe85de1b789c9d071e40479332e198b
jeongyongman <i1683u@gmail.com>
2021-10-18 09:21:15 +0900

    this

---
+++ this.js
@@ -0,0 +1,13 @@
+let kim = {
+    name: 'kim',
+    first: 10,
+    second: 20,
+    // sum:function(f,s){
+    //     return f+s;
+    // }
+    sum: function(){
+        return this.first+this.second;
+    }
+}
+// console.log(kim.sum(kim.first,kim.second));
+console.log("kim.sum()",kim.sum());
