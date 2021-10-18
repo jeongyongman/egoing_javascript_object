9a31fa9d630c603fa3b1256898e31f0d39657f12
jeongyongman <i1683u@gmail.com>
2021-10-18 17:27:20 +0900

    class

---
+++ class.js
@@ -0,0 +1,18 @@
+class Person {
+    constructor(name,first,second){
+        this.name=name;
+        this.first=first;
+        this.second=second;
+        console.log('constructor');
+    }
+}
+
+
+let kim = new Person('kim',10,20);
+console.log('kim',kim);
+// kim.sum = function(){
+//     return 'this : '+(this.first+this.second+this.third);
+// }
+// let lee = new Person('lee', 10, 10, 10);
+// console.log("kim.sum()",kim.sum());
+// console.log("lee.sum()",lee.sum());
--- prototype.js
+++ prototype.js
@@ -1,13 +1,15 @@
 function Person(name,first,second,third){
+    // 초기상태를 설정한다
     this.name= name;
     this.first= first;
     this.second= second;
     this.third= third;
 }
+// 객체를 만든다
 Person.prototype.sum = function(){
     return 'prototype : '+(this.first+this.second+this.third);
 }
-let kim = new Person('kim', 10, 20, 30);
+let kim = new Person('kim', 10, 20, 30); // 생성자 construct
 kim.sum = function(){
     return 'this : '+(this.first+this.second+this.third);
 }
