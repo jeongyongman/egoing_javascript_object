d58fbfebbc53890f06ff04c52e405ecd47634a3c
jeongyongman <i1683u@gmail.com>
2021-10-19 08:36:13 +0900

    bind

--- object_function.js
+++ object_function.js
@@ -1,13 +1,16 @@
 let kim = { name:'kim',first:10,second:20};
 let lee = { name:'lee',first:10,second:10};
-function sum() {
-    return this.first + this.second;
-}
-sum.call(kim);
-console.log("sum.call(kim)",sum.call(kim));
-console.log("sum.call(lee)",sum.call(lee));
+// function sum() {
+//     return this.first + this.second;
+// }
+// sum.call(kim);
+// console.log("sum.call(kim)",sum.call(kim));
+// console.log("sum.call(lee)",sum.call(lee));
 function sumprefix(prefix) {
     return prefix+(this.first + this.second);
 }
-console.log("sumprefix.call(kim,'=>')",sumprefix.call(kim,'=>'));
-console.log("sumprefix.call(lee,':')",sumprefix.call(lee,':'));
+// console.log("sumprefix.call(kim,'=>')",sumprefix.call(kim,'=>'));
+// console.log("sumprefix.call(lee,':')",sumprefix.call(lee,':'));
+
+let kimSum = sumprefix.bind(kim, '-> ');
+console.log('kimSum()', kimSum());
