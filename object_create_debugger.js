6c3854d29f8e5822a6e8d46a94e34504bcb5b224
jeongyongman <i1683u@gmail.com>
2021-10-19 08:05:08 +0900

    객체상속

--- object_create_debugger.js
+++ object_create_debugger.js
@@ -6,3 +6,29 @@ subObj.subVal='sub';
 debugger;
 console.log('subObj.subVal =>',subObj.subVal);
 console.log('subObj.superVal =>',subObj.superVal);
+
+let kim = {
+    name: 'kim',
+    first: 10,
+    second: 20,
+    sum:function(){return this.first+this.second}
+}
+// let lee = {
+//     name:'lee',
+//     first:10,
+//     second:10,
+//     avg: function(){return (this.first+this.second)/2}
+// }
+
+// console.log('kim.sum() : ',kim.sum());
+// lee.__proto__=kim;
+
+let lee = Object.create(kim);
+lee.name= 'lee';
+lee.first=10;
+lee.second=10;
+lee.avg=function(){
+    return (this.first+this.second)/2;
+}
+console.log('lee.sum() : ',lee.sum());
+console.log('lee.avg() : ',lee.avg());
